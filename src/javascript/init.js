bc3f2c2124ba42cc90b53cc4bb53bc3a9ce00219
hduchesne <hduchesne@jahia.com>
2023-10-19 17:03:03 +0200

    update url -> add commented code use to manage image sizes create a view for keepeek imager ref. write the doc

--- src/javascript/init.js
+++ src/javascript/init.js
@@ -27,7 +27,6 @@ export default function () {
                     description: 'keepicker:label.selectorConfig.description',
                     module:'keepicker',
                     icon: svgKeepeekLogo,
-
                 });
 
                 registerKeePickerActions(registry);
